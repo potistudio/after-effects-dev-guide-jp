6643641fcc9b3741b82cbd704ef32583ecaf0877
POTI <potistudio@proton.me>
2026-02-16 22:06:54 +0900

    fix: Reapply SSG

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,46 +3,42 @@ import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 import { STARLIGHT_SIDEBAR } from './docs-structure.mjs';
 
-import cloudflare from '@astrojs/cloudflare';
-
 // https://astro.build/config
 export default defineConfig({
-  output: 'static',
-
-  integrations: [
-      starlight({
-          title: 'After Effects C++ SDK ガイド（日本語）',
-          favicon: '/favicon.ico',
-          head: [
-              {
-                  tag: 'link',
-                  attrs: {
-                      rel: 'icon',
-                      type: 'image/png',
-                      href: '/favicon.png',
-                  },
-              },
-              {
-                  tag: 'link',
-                  attrs: {
-                      rel: 'apple-touch-icon',
-                      href: '/favicon.png',
-                  },
-              },
-          ],
-          social: [
-              {
-                  icon: 'github',
-                  label: 'GitHub',
-                  href: 'https://github.com/docsforadobe/after-effects-plugin-guide',
-              },
-          ],
-          components: {
-              Head: './src/components/StarlightHead.astro',
-          },
-          sidebar: STARLIGHT_SIDEBAR,
-      }),
+	output: 'static',
+	site: 'https://aeddjp.poti.studio',
+	integrations: [
+		starlight({
+			title: 'After Effects C++ SDK ガイド（日本語）',
+			favicon: '/favicon.ico',
+			head: [
+				{
+					tag: 'link',
+					attrs: {
+						rel: 'icon',
+						type: 'image/png',
+						href: '/favicon.png',
+					},
+				},
+				{
+					tag: 'link',
+					attrs: {
+						rel: 'apple-touch-icon',
+						href: '/favicon.png',
+					},
+				},
+			],
+			social: [
+				{
+					icon: 'github',
+					label: 'GitHub',
+					href: 'https://github.com/docsforadobe/after-effects-plugin-guide',
+				},
+			],
+			components: {
+				Head: './src/components/StarlightHead.astro',
+			},
+			sidebar: STARLIGHT_SIDEBAR,
+		}),
 	],
-
-  adapter: cloudflare(),
 });

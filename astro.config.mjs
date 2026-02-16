4ac6a8f5f13d8763e29d5a383f3392ded5cd4805
POTI <potistudio@proton.me>
2026-02-16 21:43:46 +0900

    chore: Install Cloudflare wrangler

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,22 +3,46 @@ import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 import { STARLIGHT_SIDEBAR } from './docs-structure.mjs';
 
+import cloudflare from '@astrojs/cloudflare';
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'After Effects C++ SDK ガイド（日本語）',
-			social: [
-				{
-					icon: 'github',
-					label: 'GitHub',
-					href: 'https://github.com/docsforadobe/after-effects-plugin-guide',
-				},
-			],
-			components: {
-				Head: './src/components/StarlightHead.astro',
-			},
-			sidebar: STARLIGHT_SIDEBAR,
-		}),
+  output: 'static',
+
+  integrations: [
+      starlight({
+          title: 'After Effects C++ SDK ガイド（日本語）',
+          favicon: '/favicon.ico',
+          head: [
+              {
+                  tag: 'link',
+                  attrs: {
+                      rel: 'icon',
+                      type: 'image/png',
+                      href: '/favicon.png',
+                  },
+              },
+              {
+                  tag: 'link',
+                  attrs: {
+                      rel: 'apple-touch-icon',
+                      href: '/favicon.png',
+                  },
+              },
+          ],
+          social: [
+              {
+                  icon: 'github',
+                  label: 'GitHub',
+                  href: 'https://github.com/docsforadobe/after-effects-plugin-guide',
+              },
+          ],
+          components: {
+              Head: './src/components/StarlightHead.astro',
+          },
+          sidebar: STARLIGHT_SIDEBAR,
+      }),
 	],
+
+  adapter: cloudflare(),
 });
